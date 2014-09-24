8f16d038760d1d048a62a24e99a1395d64277666
Gurdas Nijor <nijor22@gmail.com>
2014-09-24 11:28:22 -0700

    Dynamically generate sample jsx markup string + get displayName from component descriptor constructor

--- app.jsx
+++ app.jsx
@@ -27,6 +27,9 @@ var DevCard = React.createClass({
 
   render:function(){
     var component =  React.Children.only(this.props.children);
+    var name = component.constructor.displayName;
+    var propString = _.map(this.state ,function(val,key){ return key + "={" + val + "}";}).join(" ");
+    var markupString = "<" + name + " " + propString + " />";
 
     var boundInputs = !_.isEmpty(this.state) ? _.map(this.state ,function(val,key){ return  (<fieldset className="ui-form">  <label>{key}</label> <input type="text" valueLink={this.linkState(key)} />   </fieldset>);  }, this) : null;
 
@@ -35,18 +38,15 @@ var DevCard = React.createClass({
         <div className="ui-card $modifier_class" style={{height:this.props.height}}>
           <div className="ui-card__content ui-form">
           <div className="ui-width-wrapper">
-            <h3>{this.props.title}</h3>
+            <h3>{name}</h3>
             <div className="ui-row">
               <div className="ui-col-2of6">
                  {React.addons.cloneWithProps(component, _.clone(this.state))}
               </div>
               <div className="ui-col-4of6">
                 <p> {this.props.description} </p>
-
                  {boundInputs}
-
-
-                <pre> <code> {this.props.markup}  </code>  </pre></div>
+                <pre> <code> {markupString}  </code>  </pre></div>
               </div>
             </div>
           </div>
@@ -59,12 +59,6 @@ var DevCard = React.createClass({
 
 
 var App = React.createClass({
-  mixins: [React.addons.LinkedStateMixin],
-
-  getInitialState:function(){
-    return {firstName:"john",email:"asdfasd", id:0};
-  },
-
   render:function(){
     return (
       <div className="ui-width-wrapper">
@@ -82,20 +76,20 @@ var App = React.createClass({
             </div>
           </div>
 
-            <DevCard title="Calendar" initState={{minSelectionMode:"days"}} markup={"<Calendar prop1={val} />"} description="A component for allowing the selection of a day." >
+            <DevCard initState={{minSelectionMode:"days"}}  description="A component for allowing the selection of a day." >
               <Ovid.Calendar />
             </DevCard>
 
-            <DevCard title="Table" initState={{columns:[{key: 'field1', name: 'Field1', sortable: true}, {key: 'field2', name: 'Field2', sortable: true}, {key: 'field3', name: 'Field3', sortable: true}], rows:[{field1:'value1',field2:'value2',field3:'value3'},{field1:'value1',field2:'value2',field3:'value3'},{field1:'value1',field2:'value2',field3:'value3'}]}} markup={"<Table columns={array} rows={array}/>"} description="Renders small tables of data; main use as a user input control when used with the CheckboxColumn">
+            <DevCard initState={{columns:[{key: 'field1', name: 'Field1', sortable: true}, {key: 'field2', name: 'Field2', sortable: true}, {key: 'field3', name: 'Field3', sortable: true}], rows:[{field1:'value1',field2:'value2',field3:'value3'},{field1:'value1',field2:'value2',field3:'value3'},{field1:'value1',field2:'value2',field3:'value3'}]}} description="Renders small tables of data; main use as a user input control when used with the CheckboxColumn">
               <Ovid.Table/>
             </DevCard>
 
 
-            <DevCard title="Date Picker"  initState={{minSelectionMode:"months"}}  height={320} markup={"<DatePicker prop1={val} />"} description="Uses the Calendar for allowing a user to select a date.  Has modes for 'days', 'months' and 'years" >
+            <DevCard  initState={{minSelectionMode:"months"}}  height={320} description="Uses the Calendar for allowing a user to select a date.  Has modes for 'days', 'months' and 'years" >
               <Ovid.DatePicker />
             </DevCard>
 
-            <DevCard title="Avatar" initState={{id:7, email:"johndoe@gmail.com", firstName:"john", lastName:"doe"}} markup={"<Avatar prop1={val} />"} description="Displays one the following three options in order of precedence: (1) Gravatar associated with email: Show Gravatar image.  (2) First or last name provided: Show initials.  (3) Otherwise show anonymous image " >
+            <DevCard initState={{id:7, email:"johndoe@gmail.com", firstName:"john", lastName:"doe"}} description="Displays one the following three options in order of precedence: (1) Gravatar associated with email: Show Gravatar image.  (2) First or last name provided: Show initials.  (3) Otherwise show anonymous image " >
               <Ovid.Avatar />
             </DevCard>
 
