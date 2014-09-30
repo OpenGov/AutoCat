8ec77cb61d9011888b0ff123e79738a69e316ca1
Gurdas Nijor <nijor22@gmail.com>
2014-09-29 16:55:29 -1000

    Added way to push type info for hosted component into DevCard which allows the correct input type to be rendered

--- app.jsx
+++ app.jsx
@@ -3,9 +3,9 @@
 
 "use strict";
 
-var React = require('react/addons');
+var React = window.React = require('react/addons');
 var Ovid = require('Ovid');
-var _ = require('lodash');
+var _ = window._ =  require('lodash');
 
 
 
@@ -28,10 +28,12 @@ var DevCard = React.createClass({
   render:function(){
     var component =  React.Children.only(this.props.children);
     var name = component.constructor.displayName;
-    var propString = _.map(this.state ,function(val,key){ return key + "={" + val + "}";}).join(" ");
+    var propString = _.map(this.state ,function(val,key){ return key + "={" + val.data + "}";}).join(" ");
     var markupString = "<" + name + " " + propString + " />";
 
-    var boundInputs = !_.isEmpty(this.state) ? _.map(this.state ,function(val,key){ return  (<fieldset className="ui-form">  <label>{key}</label> <input type="text" valueLink={this.linkState(key)} />   </fieldset>);  }, this) : null;
+    var boundInputs = !_.isEmpty(this.state) ? _.map(this.state ,function(val,field){
+      return this.renderTypedInput(field);
+    }, this) : null;
 
     return(
       <div className="ui-card__content" >
@@ -41,7 +43,7 @@ var DevCard = React.createClass({
             <h3>{name}</h3>
             <div className="ui-row">
               <div className="ui-col-2of6">
-                 {React.addons.cloneWithProps(component, _.clone(this.state))}
+                 {React.addons.cloneWithProps(component, _.mapValues(this.state,function(v,k){ return v.data;}) )}
               </div>
               <div className="ui-col-4of6">
                 <p> {this.props.description} </p>
@@ -51,9 +53,59 @@ var DevCard = React.createClass({
             </div>
           </div>
         </div>
+
+      {this.state.errors ? JSON.stringify(this.state.errors) : null}
+
       </div>
     );
+  },
+
+  renderTypedInput:function(field){
+
+    var type = this.state[field]['type'];
+
+    var changeHandler = function(evt){
+      var val = evt.target.value;
+      var newData = {};
+
+      if (type === "array" || type === "object"){
+        try {
+          val = JSON.parse(val);
+
+          newData[field] = {data:val, type:type};
+
+          this.setState(newData);
+
+        }
+        catch(e){
+          //TODO: figure out how to handle invalid input being live-entered -- could mean not using 'controlled components'?
+          var errColl = this.state.errors ?  _.cloneDeep(this.state.errors) : [];
+          errColl.push(e.message);
+          this.setState({errors:errColl});
+
+        }
+      }
+
+
+
+    }.bind(this);
+
+
+    var inputVal = (type === "array" || type === "object") ? JSON.stringify(this.state[field]['data']) : this.state[field]['data'];
+
+
+    var inputTypeHash = {array:"text", object:"text", string:"text", date:"date", number:"number"};
+
+
+    return (
+      <fieldset className="ui-form">
+        <label>{field}</label>
+
+        <input type={inputTypeHash[type]} onChange={changeHandler}  value={inputVal} />
+      </fieldset>
+    );
   }
+
 });
 
 
@@ -76,20 +128,20 @@ var App = React.createClass({
             </div>
           </div>
 
-            <DevCard initState={{minSelectionMode:"days"}}  description="A component for allowing the selection of a day." >
+            <DevCard initState={{minSelectionMode:{type:"string", data:"days"}, date:{type:"date", data:new Date()} }}  description="A component for allowing the selection of a day." >
               <Ovid.Calendar />
             </DevCard>
 
-            <DevCard initState={{columns:[{key: 'field1', name: 'Field1', sortable: true}, {key: 'field2', name: 'Field2', sortable: true}, {key: 'field3', name: 'Field3', sortable: true}], rows:[{field1:'value1',field2:'value2',field3:'value3'},{field1:'value1',field2:'value2',field3:'value3'},{field1:'value1',field2:'value2',field3:'value3'}]}} description="Renders small tables of data; main use as a user input control when used with the CheckboxColumn">
+            <DevCard initState={{columns:{type:"array", data:[{key: 'field1', name: 'Field1', sortable: true}, {key: 'field2', name: 'Field2', sortable: true}, {key: 'field3', name: 'Field3', sortable: true}]}, rows:{type:"array", data:[{field1:'value1',field2:'value2',field3:'value3'},{field1:'value1',field2:'value2',field3:'value3'},{field1:'value1',field2:'value2',field3:'value3'}]}}} description="Renders small tables of data; main use as a user input control when used with the CheckboxColumn">
               <Ovid.Table/>
             </DevCard>
 
 
-            <DevCard  initState={{minSelectionMode:"months"}}  height={320} description="Uses the Calendar for allowing a user to select a date.  Has modes for 'days', 'months' and 'years" >
+            <DevCard  initState={{minSelectionMode:{type:"string", data:"months"}, date:{type:"date", data:new Date()} }}  height={320} description="Uses the Calendar for allowing a user to select a date.  Has modes for 'days', 'months' and 'years" >
               <Ovid.DatePicker />
             </DevCard>
 
-            <DevCard initState={{id:7, email:"johndoe@gmail.com", firstName:"john", lastName:"doe"}} description="Displays one the following three options in order of precedence: (1) Gravatar associated with email: Show Gravatar image.  (2) First or last name provided: Show initials.  (3) Otherwise show anonymous image " >
+            <DevCard initState={{id:{type:"number",data:7}, email:{type:"string", data:"johndoe@gmail.com"}, firstName:{type:"string", data:"john"}, lastName:{type:"string", data:"doe"}}} description="Displays one the following three options in order of precedence: (1) Gravatar associated with email: Show Gravatar image.  (2) First or last name provided: Show initials.  (3) Otherwise show anonymous image " >
               <Ovid.Avatar />
             </DevCard>
 
